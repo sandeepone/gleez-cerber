3866ebc818244d834c90d9b05ace1250e9a32369
Sergey Yakovlev <sadhooklay@gmail.com>
2013-04-15 01:06:45 +0400

    New feature: Added initial Views for pages

---
+++ views/page/list.php
@@ -0,0 +1,17 @@
+<?php defined('SYSPATH') OR die('No direct script access.'); ?>
+
+<?php foreach($posts as $i => $post): ?>
+	<div id="post-<?php echo $i; ?>" class="post-list <?php echo ($post->sticky) ? ' sticky' : ' post-'. $post->status; ?>">
+		<h2 class="post-title">
+			<?php echo HTML::anchor($post->url, $post->title); ?>
+		</h2>
+		<?php
+		echo View::factory($post->type.'/teaser')
+			->set('post',       $post)
+			->set('config',     $config)
+			->set('page_title', TRUE);
+		?>
+	</div>
+<?php endforeach; ?>
+
+<?php echo $pagination; ?>
---
+++ views/page/post.php
@@ -0,0 +1,26 @@
+<?php defined("SYSPATH") OR die("No direct script access.") ?>
+
+<div class="post-page-wrapper">
+	<?php if (isset($page)): ?>
+		<div class="post-page"><?php echo $page; ?></div>
+	<?php endif;?>
+</div>
+
+<div class="list-comments">
+	<?php if (isset($comments)): ?>
+		<div class="post-comments"><?php echo $comments; ?></div>
+	<?php endif;?>
+</div>
+
+<div class="post-comment-form-wrapper">
+	<?php if (isset($provider_buttons)): ?>
+		<div id="post-provider-buttons">
+			<?php echo $provider_buttons; ?>
+			<?php echo Form::textarea('comment', '', array('disabled' => TRUE, 'class' => 'textarea full', 'rows' => 5)); ?>
+		</div>
+	<?php endif;?>
+
+	<?php if (isset($comment_form)): ?>
+		<div class="post-comment-form"><?php echo $comment_form; ?></div>
+	<?php endif;?>
+</div>
