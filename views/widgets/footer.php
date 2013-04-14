edf6b52bc207d9c5908cefbf5ad3360da386c788
Sergey Yakovlev <sadhooklay@gmail.com>
2013-04-14 05:27:51 +0400

    Updated feature: Modified footer blocks

--- views/widgets/footer.php
+++ views/widgets/footer.php
@@ -1,13 +1,10 @@
 <?php defined('SYSPATH') OR die('No direct script access.'); ?>
 
-<div id="widget-<?php echo $widget->module; ?>-<?php echo $widget->name; ?> <?php echo (isset($id)) ? 'widget-'.$id : '' ?>" class="widget blockme widget-<?php echo $widget->name; ?> <?php echo ($widget->menu) ? 'widget-menu' : ''; ?>">
-
+<div class="span3 footer-block">
     <?php if ($widget->show_title): ?>
-        <div class="widget-header">
-            <h3><?php echo Text::plain($title); ?></h3>
-        </div>
+        <h4><?php echo Text::plain($title); ?></h4>
     <?php endif; ?>
-
-    <div class="widget-content"><?php echo $content; ?></div>
-
+    <div class="footer-block-content">
+	    <?php echo $content; ?>
+    </div>
 </div>
