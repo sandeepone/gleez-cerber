b3c354b17571e83ec66e5377b01bc92a3b6c52eb
Sergey Yakovlev <sadhooklay@gmail.com>
2013-04-25 02:02:58 +0400

    New feature: Added RSS link to page list

--- views/page/list.php
+++ views/page/list.php
@@ -1,5 +1,10 @@
 <?php defined('SYSPATH') OR die('No direct script access.'); ?>
 
+<div class="row">
+	<div class="span1 pull-right">
+		<?php echo HTML::icon($feed, 'icon-rss', array('title' => 'RSS 2.0', 'class' => 'pull-right')); ?>
+	</div>
+</div>
 <?php foreach($posts as $i => $post): ?>
 	<div id="post-<?php echo $i; ?>" class="post-list <?php echo ($post->sticky) ? ' sticky' : ' post-'. $post->status; ?>">
 		<h2 class="post-title">
