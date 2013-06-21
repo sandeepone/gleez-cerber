be4ceb122d9132366f15fb407b9cf5218866db35
Sergey Yakovlev <sadhooklay@gmail.com>
2013-06-21 19:26:29 +0400

    Fixed issue: Added missed widgets to main page

--- views/welcome.php
+++ views/welcome.php
@@ -11,6 +11,8 @@
 				<p>GleezCMS is a user-friendly website content management system.  With GleezCMS you can easily build dynamic websites within a matter of minutes with just the click of your mouse!  Maintain your web content, navigation and even limit what groups or specific users can access, from anywhere in the world with just a web browser!  With an emphasis on security and functionality, GleezCMS is a professional and robust system suitable for any business or organization website.  Built on the PHP programming language and the MySQL database, GleezCMS delivers superb performance on any size website.</p>
 			</div>
 		</div>
-	<?php endif; ?>
+	<?php else: ?>
+		<?php echo $widgets ?>
+	<?php endif ?>
     <!-- Frontpage Region End -->
 </div>
