e821388890cff62274e0bee90c83483dc822ed21
Sandeep Sangamreddi <sandeepone@gmail.com>
2013-11-12 02:59:32 +0530

    Fixed issue: Fix loginform consistent and cleanup

--- views/user/login.php
+++ views/user/login.php
@@ -1,52 +1,40 @@
 <?php defined('SYSPATH') OR die('No direct script access.'); ?>
 <?php Assets::css('user', 'media/css/user.css', array('weight' => 2)); ?>
 
-<div class="account-container">
+<div class="account-container col-md-4">
 	<?php include Kohana::find_file('views', 'errors/partial'); ?>
 
 	<div class="content clearfix">
-		<?php echo Form::open($action, array('class' => 'row')); ?>
+		<?php echo Form::open($action, array('class' => 'row', 'role' => 'form')); ?>
 			<h1><?php echo $site_name ?></h1>
 
 			<div class="login-fields">
-				<p><?php echo __('Sign in using your registered account:'); ?></p>
+				<p><?php echo __('Sign in using your registered account'); ?></p>
 
-				<div class="field">
-					<div class="control-group <?php echo isset($errors['name']) ? 'error': ''; ?>">
-						  <div class="controls">
-							<div class="input-prepend">
-							  <span class="add-on"><i class="icon-large icon-user"></i></span>
-							  <?php echo Form::input('name', $post->name, array('placeholder' => __('Username/Email'))); ?>
-							</div>
-						  </div>
-					</div>
+				<div class="form-group <?php echo isset($errors['name']) ? 'has-error': ''; ?>">
+					<?php echo Form::input('name', $post->name, array('class' =>'form-control', 'placeholder' => __('Email'))); ?>
 				</div>
 
-				<div class="field">
-					<div class="control-group <?php echo isset($errors['password']) ? 'error': ''; ?>">
-						<div class="controls">
-							<div class="input-prepend">
-								<span class="add-on"><i class="icon-large icon-key"></i></span>
-								<?php echo Form::password('password', NULL, array('placeholder' => __('Password'))); ?>
-							</div>
-						</div>
-					</div>
+				<div class="form-group <?php echo isset($errors['password']) ? 'has-error': ''; ?>">
+					<?php echo Form::password('password', NULL, array('class' =>'form-control', 'placeholder' => __('Password'))); ?>
 				</div>
 			</div>
 
 			<div class="login-actions">
+				<?php echo Form::submit('login', __('Login'), array('class' => 'btn btn-primary btn-lg btn-block', 'type' => 'submit')); ?>
+				<div class="clearfix"></div><br>
 				<span class="login-checkbox">
 					<input id="remember" name="remember" type="checkbox" class="field login-checkbox" value="TRUE" tabindex="4">
 					<label class="choice" for="remember"><?php echo __('Stay Signed in'); ?></label>
 				</span>
-				<?php echo Form::button('login', __('Login'), array('class' => 'btn btn-primary pull-right', 'type' => 'submit')); ?>
+				<?php echo HTML::anchor('user/reset/password', __('Forgot Password?'), array('class' => 'pull-right')); ?>
 				<div class="clearfix"></div><br>
-				<ul>
-					<li><?php echo HTML::anchor('user/reset/password', __('Forgot Password?')); ?></li>
-			 		<?php if ($register): ?>
-						<li><?php echo HTML::anchor('user/register', __("Don't have an account?")); ?></li>
-					<?php endif; ?>
-				</ul>
+
+			 	<?php if ($register): ?>
+			 		<div class="text-center">
+						<?php echo HTML::anchor('user/register', __("Don't have an account?")); ?>
+					</div>
+				<?php endif; ?>
 			</div>
 
 			<?php if ($providers): ?>
