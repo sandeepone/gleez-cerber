e72d4b9b9da9434491e53cd8fc619064480d55a8
Sandeep Sangamreddi <sandeepone@gmail.com>
2013-11-14 17:13:36 +0530

    Added layouts and admin equal columns

--- views/admin.tpl.php
+++ views/admin.tpl.php
@@ -1,28 +1,28 @@
-<?php if ($messages): ?>
-	<!-- ########## Messages start ########## -->
-	<div id="messages" class="messages span8 offset2">
-		<?php echo $messages ?>
-	</div>
-	<!-- ########## Messages end ########## -->
-<?php endif; ?>
-
-<div class="row-fluid">
-	<?php if ($sidebar_left): ?>
-		<!-- ########## Sidebar left start ########## -->
-		<div class="span3">
-			<div id="left">
-				<?php echo $sidebar_left; ?>
-			</div>
+<div class="content row">
+	<?php if ($sidebar_left || $sidebar_right): ?>
+		<!-- ########## Sidebar start ########## -->
+		<div class="sidebar col-sm-3 col-md-3 col-lg-2 navbar-inverse">
+			<?php echo $sidebar_left; ?>
+			<?php echo $sidebar_right; ?>
 		</div>
-		<!-- ########## Sidebar left end ########## -->
+		<!-- ########## Sidebar end ########## -->
 	<?php endif; ?>
 	<!-- ########## Content start ############# -->
-	<div id="row-content" class="span<?php echo $main_column?>">
+	<div class="main-content col-sm-9 col-md-9 col-lg-10">
 		<?php if ($title): ?>
 			<div class="page-header">
 				<h1><?php echo $title;?></h1>
 			</div>
 		<?php endif; ?>
+
+		<?php if ($messages): ?>
+			<!-- ########## Messages start ########## -->
+			<div id="messages" class="messages">
+				<?php echo $messages ?>
+			</div>
+			<!-- ########## Messages end ########## -->
+		<?php endif; ?>
+
 		<?php if ($tabs): ?>
 			<div id="tabs-actions">
 				<div id="tabs"><?php echo $tabs; ?></div>
@@ -31,15 +31,27 @@
 		<div id="content" class="<?php echo $tabs ? 'with-tabs' : 'without-tabs'?>">
 			<?php echo $content; ?>
 		</div>
-	</div>
-	<!-- ########## Content end ############### -->
-	<?php if ($sidebar_right): ?>
-		<!-- ########## Sidebar right start ########## -->
-		<div class="span3">
-			<div id="right">
-				<?php echo $sidebar_right; ?>
+
+		<div id="push"></div>
+		<!-- ########## Footer start ########## -->
+		<footer class="footer">
+			<div class="text-muted">
+				<div class="row">
+					<div class="col-xs-6 col-md-6">
+						<p class="pull-left"><?php echo __('&copy; :year :site', array(':year' => date('Y'), ':site' => HTML::anchor(URL::site(false, true), $site_name)));?></p>
+					</div>
+					<div class="col-xs-6 col-md-6">
+						<p class="pull-right"><?php echo __(':powerdby v{gleez_version}', array(':powerdby' => HTML::anchor('http://gleezcms.org/', 'Gleez CMS')))?></p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 text-center" id="footer-system-info">
+						<small><?php echo __('Rendered in {execution_time}, using {memory_usage} of memory.')?></small>
+					</div>
+				</div>
 			</div>
-		</div>
-		<!-- ########## Sidebar right end ########## -->
-	<?php endif; ?>
+		</footer>
+		<!-- ########## Footer end ########## -->
+	</div>
+
 </div>
--- views/layouts/admin.php
+++ views/layouts/admin.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="<?php echo $lang; ?>">
+<html id="backend" lang="<?php echo $lang; ?>">
 <head>
 	<title><?php echo $head_title ?></title>
 	<?php echo Meta::tags(); ?>
@@ -9,15 +9,13 @@
 		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
 	<?php echo Assets::css(); ?>
-	<!--[if lt IE 8]>
-		<link type="text/css" href="/media/css/font-awesome-ie7.css" rel="stylesheet" media="all" />
-	<![endif]-->
 </head>
 <body id="<?php echo $page_id; ?>" class="<?php echo $page_class; ?>">
 
+
 	<!-- ########## Navbar start ########## -->
     <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      	<div class="container">
+      	<div class="admin-nav container">
 	        <div class="navbar-header">
 	          	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
 		            <span class="sr-only">Toggle navigation</span>
@@ -25,10 +23,10 @@
 		            <span class="icon-bar"></span>
 		            <span class="icon-bar"></span>
 	          	</button>
-	          	<?php echo HTML::anchor('/', HTML::image($site_logo, array('alt' => $site_slogan, 'class' => 'logo')), array('class' => 'navbar-brand', 'title' => $site_name)) ?>
+	          	<?php echo HTML::anchor('/', $site_name, array('class' => 'navbar-brand', 'title' => $site_name)) ?>
 	        </div>
         	<div class="navbar-collapse collapse">
-				<?php echo $primary_menu; ?>
+
           			
           		<ul class="nav navbar-nav navbar-right">
 						<?php if (User::is_guest()): ?>
@@ -60,45 +58,11 @@
     </div>
 	<!-- ########## Navbar end ########## -->
 
-	<!-- ########## template / container-fluid start ########## -->
-	<div class="container" itemscope itemtype="http://schema.org/WebPage">
-		<?php
-			$tpl = $is_admin ? 'admin' : 'default';
-			include Kohana::find_file('views', $tpl.'.tpl');
-		?>
+	<!-- ########## admin / container start ########## -->
+	<div class="admin-container container" itemscope itemtype="http://schema.org/WebPage">
+		<?php include Kohana::find_file('views', 'admin.tpl'); ?>
 	</div>
-	<!-- ########## template / container-fluid end ########## -->
-	<!-- ########## Footer start ########## -->
-	<footer class="footer">
-		<?php $footer = Widgets::instance()->render('footer', 'footer'); ?>
-		<?php if ($footer): ?>
-			<div class="extra">
-				<div class="container">
-					<div class="row">
-						<?php echo $footer; ?>
-					</div>
-				</div>
-			</div>
-		<?php endif; ?>
-		<div class="footer-terms">
-			<div class="container text-muted">
-				<div class="row">
-					<div class="col-xs-6 col-md-6">
-						<p class="pull-left"><?php echo __('&copy; :year :site', array(':year' => date('Y'), ':site' => HTML::anchor(URL::site(false, true), $site_name)));?></p>
-					</div>
-					<div class="col-xs-6 col-md-6">
-						<p class="pull-right"><?php echo __(':powerdby v{gleez_version}', array(':powerdby' => HTML::anchor('http://gleezcms.org/', 'Gleez CMS')))?></p>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-12 text-center" id="footer-system-info">
-						<small><?php echo __('Rendered in {execution_time}, using {memory_usage} of memory.')?></small>
-					</div>
-				</div>
-			</div>
-		</div>
-	</footer>
-	<!-- ########## Footer end ########## -->
+	<!-- ########## template / container end ########## -->
 
 	<?php echo Assets::js(FALSE); ?>
 	<?php echo Assets::codes(FALSE); ?>
