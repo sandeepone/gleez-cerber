01518f616238f70e117f136ecf24c1c6c53b2648
Sergey Yakovlev <sadhooklay@gmail.com>
2013-11-15 04:46:57 +0400

    Updated feature: Updated user/edit page. Note: For tabbed content should use this as example

--- views/user/edit.php
+++ views/user/edit.php
@@ -1,99 +1,116 @@
 <div class="span12">
-	<div class="widget">
-		<?php include Kohana::find_file('views', 'errors/partial'); ?>
+	<?php include Kohana::find_file('views', 'errors/partial'); ?>
 
-		<div class="widget-header">
-			<span class="icon-user"></span>
-			<h3><?php echo __('User Account')?></h3>
-		</div>
+	<?php echo Form::open($action, array('class' => 'form-horizontal', 'role' => 'form')); ?>
 
-		<div class="widget-content">
-			<div class="tabbable">
-				<ul class="nav nav-tabs" id="profile-tabs">
-					<li class="active">
-						<?php echo HTML::anchor('#profile', __('Profile'), array('data-toggle' => 'tab')) ?>
-					</li>
-					<li>
-						<?php echo HTML::anchor('#settings', __('Settings'), array('data-toggle' => 'tab')) ?>
-					</li>
-				</ul>
-				<br>
-				<?php echo Form::open($action, array('class' => 'form-horizontal')); ?>
-				<div class="tab-content" id="profile-tabs-content">
-					<div class="tab-pane fade in active" id="profile">
-						<div class="control-group <?php echo isset($errors['nick']) ? 'error': ''; ?>">
-							<?php echo Form::label('nick', __('Display Name'), array('class' => 'control-label')) ?>
-							<div class="controls">
-								<div class="input-prepend">
-									<span class="add-on"><i class="icon-user"></i></span>
-									<?php echo Form::input('nick', $user->nick, array('class' => 'input-xlarge')); ?>
-								</div>
-							</div>
-						</div>
-						<div class="control-group <?php echo isset($errors['gender']) ? 'error': ''; ?>">
-							<?php echo Form::label('gender', __('Gender'), array('class' => 'control-label')) ?>
-							<div class="controls">
-								<?php echo Form::label('gender1', Form::radio('gender', 1, $male).__('Male'), array('class' => 'radio')) ?>
-								<?php echo Form::label('gender2', Form::radio('gender', 2, $female).__('Female'), array('class' => 'radio')) ?>
-							</div>
-						</div>
+	<ul class="nav nav-tabs col-sm-12" id="profile-tabs">
+		<li class="active"><a href="#profile" data-toggle="tab"><?php _e('Profile')?></a></li>
+		<li><a href="#settings" data-toggle="tab"><?php _e('Settings')?></a></li>
+	</ul>
+	<div class="tab-content" id="profile-tabs-content">
+		<div class="tab-pane fade in active" id="profile">
 
-						<div class="control-group <?php echo isset($errors['dob']) ? 'error': ''; ?>">
-							<?php echo Form::label('dob', __('Birthday'), array('class' => 'control-label')) ?>
-							<div class="controls">
-								<?php echo Form::select('month', Date::months(Date::MONTHS_SHORT), date('n', $user->dob), array('class' => 'input-small')); ?>
-								<?php echo Form::select('days',  Date::days(Date::DAY), date('j', $user->dob), array('class' => 'input-small')); ?>
-								<?php echo Form::select('years', Date::years(date('Y') - 95,date('Y') - 5), date('Y', $user->dob), array('class' => 'input-small')); ?>
-							</div>
-						</div>
-
-						<div class="control-group <?php echo isset($errors['homepage']) ? 'error': ''; ?>">
-							<?php echo Form::label('homepage', __('Home Page'), array('class' => 'control-label')) ?>
-							<div class="controls">
-								<div class="input-prepend">
-									<span class="add-on"><i class="icon-link"></i></span>
-									<?php echo Form::input('homepage', $user->homepage, array('class' => 'input-xlarge', 'rel' => 'tooltip', 'data-placement' => 'right', 'title' => __('Will be public'))); ?>
-								</div>
-								<?php if ($user->homepage): ?>
-									<span class="help-inline"><?php echo HTML::anchor($user->homepage, __('Go to'), array('target' => 'blank')) ?></span>
-								<?php endif; ?>
-							</div>
-						</div>
-
-						<div class="control-group <?php echo isset($errors['bio']) ? 'error': ''; ?>">
-							<?php echo Form::label('bio', __('Bio'), array('class' => 'control-label')) ?>
-							<div class="controls">
-								<?php echo Form::textarea('bio', $user->bio, array('class' => 'input-xlarge', 'rows' => 5)); ?>
-								<span class="help-block"><?php echo __('Public field. No more than 800 characters') ?></span>
-							</div>
-						</div>
-					</div>
-					<div class="tab-pane fade" id="settings">
-						<div class="control-group <?php echo isset($errors['name']) ? 'error': ''; ?>">
-							<?php echo Form::label('name', __('Username'), array('class' => 'control-label')) ?>
-							<div class="controls">
-								<div class="input-prepend">
-									<span class="add-on"><i class="icon-lock"></i></span>
-									<?php echo Form::input('name', $user->name, array('class' => 'input-large disabled', 'disabled')); ?>
-								</div>
-								<span class="help-block"><?php echo __('Username for logging in can\'t be changed.') ?></span>
-							</div>
-						</div>
-						<div class="control-group <?php echo isset($errors['mail']) ? 'error': ''; ?>">
-							<?php echo Form::label('mail', __('Mail'), array('class' => 'control-label')) ?>
-							<div class="controls">
-								<div class="input-prepend">
-									<span class="add-on"><i class="icon-envelope"></i></span>
-									<?php echo Form::input('mail', $user->mail, array('class' => 'input-large',   'rel' => 'tooltip', 'data-placement' => 'right', 'title' => __('Will be private'))); ?>
-								</div>
-							</div>
+			<div class="form-group <?php echo isset($errors['nick']) ? 'error': ''; ?>">
+				<?php echo Form::label('nick', __('Display Name'), array('class' => 'col-sm-3 control-label')) ?>
+				<div class="col-sm-9">
+					<div class="row">
+						<div class="input-group col-xs-6">
+							<span class="input-group-addon"><i class="fa fa-user"></i></span>
+							<?php echo Form::input('nick', $user->nick, array('class' => 'form-control', 'rel' => 'tooltip', 'data-placement' => 'right', 'title' => __('Will be public'))); ?>
 						</div>
 					</div>
 				</div>
-				<?php echo Form::button('user_edit', __('Save'), array('class' => 'btn btn-success pull-right', 'type' => 'submit'))?>
-				<div class="clearfix"></div><br>
-				<?php echo Form::close() ?>
 			</div>
+
+			<div class="form-group <?php echo isset($errors['gender']) ? 'error': ''; ?>">
+				<?php echo Form::label('gender', __('Gender'), array('class' => 'col-sm-3 control-label')) ?>
+				<div class="col-sm-9">
+					<div class="radio"
+						<?php echo Form::label('gender1', Form::radio('gender', 1, $male).__('Male')) ?>
+					</div>
+					<div class="radio"
+						<?php echo Form::label('gender2', Form::radio('gender', 2, $female).__('Female')) ?>
+					</div>
+				</div>
+			</div>
+
+			<div class="form-group <?php echo isset($errors['dob']) ? 'error': ''; ?>">
+				<?php echo Form::label('dob', __('Birthday'), array('class' => 'col-sm-3 control-label')) ?>
+				<div class="col-sm-9">
+					<div class="row">
+						<div class="col-xs-2">
+							<?php echo Form::select('month', Date::months(Date::MONTHS_SHORT), date('n', $user->dob), array('class' => 'form-control input-sm')) ?>
+						</div>
+						<div class="col-xs-2">
+							<?php echo Form::select('days',  Date::days(Date::DAY), date('j', $user->dob), array('class' => 'form-control input-sm')) ?>
+						</div>
+						<div class="col-xs-2">
+							<?php echo Form::select('years', Date::years(date('Y') - 95,date('Y') - 5), date('Y', $user->dob), array('class' => 'form-control input-sm')) ?>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="form-group <?php echo isset($errors['homepage']) ? 'error': ''; ?>">
+				<?php echo Form::label('homepage', __('Home Page'), array('class' => 'col-sm-3 control-label')) ?>
+				<div class="col-sm-9">
+					<div class="row">
+						<div class="input-group col-xs-10">
+							<span class="input-group-addon"><i class="fa fa-link"></i></span>
+							<?php echo Form::input('homepage', $user->homepage, array('class' => 'form-control', 'rel' => 'tooltip', 'data-placement' => 'right', 'title' => __('Will be public'))); ?>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="form-group <?php echo isset($errors['bio']) ? 'error': ''; ?>">
+				<?php echo Form::label('bio', __('Bio'), array('class' => 'col-sm-3 control-label')) ?>
+				<div class="col-sm-9">
+					<div class="row">
+						<div class="input-group col-xs-10">
+							<?php echo Form::textarea('bio', $user->bio, array('class' => 'form-control', 'rows' => 8)); ?>
+							<span class="help-block"><?php echo __('Public field. No more than 800 characters') ?></span>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+		<div class="tab-pane fade" id="settings">
+
+			<div class="form-group <?php echo isset($errors['name']) ? 'error': ''; ?>">
+				<?php echo Form::label('name', __('Username'), array('class' => 'col-sm-3 control-label')) ?>
+				<div class="col-sm-9">
+					<div class="row">
+						<div class="input-group col-xs-6">
+							<span class="input-group-addon"><i class="fa fa-lock"></i></span>
+							<?php echo Form::input('name', $user->name, array('class' => 'form-control disabled', 'disabled')); ?>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="form-group <?php echo isset($errors['mail']) ? 'error': ''; ?>">
+				<?php echo Form::label('mail', __('Mail'), array('class' => 'col-sm-3 control-label')) ?>
+				<div class="col-sm-9">
+					<div class="row">
+						<div class="input-group col-xs-6">
+							<span class="input-group-addon"><i class="fa fa-envelope"></i></span>
+							<?php echo Form::input('mail', $user->mail, array('class' => 'form-control',  'rel' => 'tooltip', 'data-placement' => 'right', 'title' => __('Will be private'))); ?>
+						</div>
+					</div>
+				</div>
+			</div>
+
 		</div>
 	</div>
+
+	<div class="form-group" id="profile-tabs-button">
+		<div class="col-sm-12">
+			<?php echo Form::button('user_edit', __('Save'), array('class' => 'btn btn-success pull-right', 'type' => 'submit'))?>
+		</div>
+	</div>
+
+	<?php echo Form::close() ?>
 </div>
