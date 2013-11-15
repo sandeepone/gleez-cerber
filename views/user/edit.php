c5ada5e5ceac06f7ce3d705ad90e8d4d6deb62ec
Sergey Yakovlev <sadhooklay@gmail.com>
2013-11-15 14:16:08 +0400

    Fixed issue: Cleanup user/edit form

--- views/user/edit.php
+++ views/user/edit.php
@@ -1,18 +1,17 @@
-<div class="span12">
 	<?php include Kohana::find_file('views', 'errors/partial'); ?>
 
 	<?php echo Form::open($action, array('class' => 'form-horizontal', 'role' => 'form')); ?>
 
-	<ul class="nav nav-tabs col-sm-12" id="profile-tabs">
+	<ul class="nav nav-tabs col-md-12" id="profile-tabs">
 		<li class="active"><a href="#profile" data-toggle="tab"><?php _e('Profile')?></a></li>
 		<li><a href="#settings" data-toggle="tab"><?php _e('Settings')?></a></li>
 	</ul>
-	<div class="tab-content" id="profile-tabs-content">
+	<div class="tab-content col-md-12" id="profile-tabs-content">
 		<div class="tab-pane fade in active" id="profile">
 
 			<div class="form-group <?php echo isset($errors['nick']) ? 'error': ''; ?>">
-				<?php echo Form::label('nick', __('Display Name'), array('class' => 'col-sm-3 control-label')) ?>
-				<div class="col-sm-9">
+				<?php echo Form::label('nick', __('Display Name'), array('class' => 'col-md-3 control-label')) ?>
+				<div class="col-md-9">
 					<div class="row">
 						<div class="input-group col-xs-6">
 							<span class="input-group-addon"><i class="fa fa-user"></i></span>
@@ -23,8 +22,8 @@
 			</div>
 
 			<div class="form-group <?php echo isset($errors['gender']) ? 'error': ''; ?>">
-				<?php echo Form::label('gender', __('Gender'), array('class' => 'col-sm-3 control-label')) ?>
-				<div class="col-sm-9">
+				<?php echo Form::label('gender', __('Gender'), array('class' => 'col-md-3 control-label')) ?>
+				<div class="col-md-9">
 					<div class="radio"
 						<?php echo Form::label('gender1', Form::radio('gender', 1, $male).__('Male')) ?>
 					</div>
@@ -35,8 +34,8 @@
 			</div>
 
 			<div class="form-group <?php echo isset($errors['dob']) ? 'error': ''; ?>">
-				<?php echo Form::label('dob', __('Birthday'), array('class' => 'col-sm-3 control-label')) ?>
-				<div class="col-sm-9">
+				<?php echo Form::label('dob', __('Birthday'), array('class' => 'col-md-3 control-label')) ?>
+				<div class="col-md-9">
 					<div class="row">
 						<div class="col-xs-2">
 							<?php echo Form::select('month', Date::months(Date::MONTHS_SHORT), date('n', $user->dob), array('class' => 'form-control input-sm')) ?>
@@ -52,8 +51,8 @@
 			</div>
 
 			<div class="form-group <?php echo isset($errors['homepage']) ? 'error': ''; ?>">
-				<?php echo Form::label('homepage', __('Home Page'), array('class' => 'col-sm-3 control-label')) ?>
-				<div class="col-sm-9">
+				<?php echo Form::label('homepage', __('Home Page'), array('class' => 'col-md-3 control-label')) ?>
+				<div class="col-md-9">
 					<div class="row">
 						<div class="input-group col-xs-10">
 							<span class="input-group-addon"><i class="fa fa-link"></i></span>
@@ -64,8 +63,8 @@
 			</div>
 
 			<div class="form-group <?php echo isset($errors['bio']) ? 'error': ''; ?>">
-				<?php echo Form::label('bio', __('Bio'), array('class' => 'col-sm-3 control-label')) ?>
-				<div class="col-sm-9">
+				<?php echo Form::label('bio', __('Bio'), array('class' => 'col-md-3 control-label')) ?>
+				<div class="col-md-9">
 					<div class="row">
 						<div class="input-group col-xs-10">
 							<?php echo Form::textarea('bio', $user->bio, array('class' => 'form-control', 'rows' => 8)); ?>
@@ -80,8 +79,8 @@
 		<div class="tab-pane fade" id="settings">
 
 			<div class="form-group <?php echo isset($errors['name']) ? 'error': ''; ?>">
-				<?php echo Form::label('name', __('Username'), array('class' => 'col-sm-3 control-label')) ?>
-				<div class="col-sm-9">
+				<?php echo Form::label('name', __('Username'), array('class' => 'col-md-3 control-label')) ?>
+				<div class="col-md-9">
 					<div class="row">
 						<div class="input-group col-xs-6">
 							<span class="input-group-addon"><i class="fa fa-lock"></i></span>
@@ -92,8 +91,8 @@
 			</div>
 
 			<div class="form-group <?php echo isset($errors['mail']) ? 'error': ''; ?>">
-				<?php echo Form::label('mail', __('Mail'), array('class' => 'col-sm-3 control-label')) ?>
-				<div class="col-sm-9">
+				<?php echo Form::label('mail', __('Mail'), array('class' => 'col-md-3 control-label')) ?>
+				<div class="col-md-9">
 					<div class="row">
 						<div class="input-group col-xs-6">
 							<span class="input-group-addon"><i class="fa fa-envelope"></i></span>
@@ -107,10 +106,9 @@
 	</div>
 
 	<div class="form-group" id="profile-tabs-button">
-		<div class="col-sm-12">
+		<div class="col-md-12">
 			<?php echo Form::button('user_edit', __('Save'), array('class' => 'btn btn-success pull-right', 'type' => 'submit'))?>
 		</div>
 	</div>
 
 	<?php echo Form::close() ?>
-</div>
