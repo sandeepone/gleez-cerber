a1a6d6db3c63121f3b43da60cf452b1c36354ccb
Sandeep Sangamreddi <sandeepone@gmail.com>
2013-11-16 20:32:33 +0530

    Fixed issue: Minor cleanups

--- views/welcome.php
+++ views/welcome.php
@@ -31,39 +31,58 @@
 	<h3>What Is Gleez CMS?</h3>
 	<p>Gleez is an open source, <a href="http://en.wikipedia.org/wiki/Content_management_system">extensible Content Management System</a> that is built on the <a href="http://kohanaframework.org">Kohana Framework</a>. Gleez is the fusion of many good concepts from the 3 most popular CMS: Joomla, Drupal, Wordpress and Ruby Rails. With years of experience using these, we've gathered great ideas and in some cases, improved on them.</p>
 	<p>With an emphasis on security and functionality, Gleez is a professional and robust system suitable for any business or organization. Built on the PHP programming language and the MySQL database, Gleez delivers superb performance on any size application.</p>
+	
 	<div class="gleez-features">
 		<div class="row">
 			<div class="col-md-4">
-				<i class="fa fa-cogs fa-3x"></i>
+				<span class="fa-stack fa-3x">
+					<i class="fa fa-circle fa-stack-2x"></i>
+					<i class="fa fa-cogs fa-stack-1x fa-inverse"></i>
+				</span>
 				<h4>Modules</h4>
 				<p>Modules - Code that extends the functionalities of Gleez. We just call them "Modules", others call it "Plugins"(Wordpress) or "Modules"(Drupal), but it's a more general term. Everything you can use for Gleez is an Extension.</p>
 			</div>
 
 			<div class="col-md-4">
-				<i class="fa fa-camera fa-3x"></i>
+				<span class="fa-stack fa-3x">
+					<i class="fa fa-circle fa-stack-2x"></i>
+					<i class="fa fa-camera fa-stack-1x fa-inverse"></i>
+				</span>
 				<h4>Themes</h4>
 				<p>Themes - the look and layout of a Gleez. A theme consists of one or more PHP files that define the HTML output of your site's pages, along with one or more CSS files that define the layout, fonts, colors, and other styles.</p>
 			</div>
 
 			<div class="col-md-4">
-				<i class="fa fa-columns fa-3x"></i>
+				<span class="fa-stack fa-3x">
+					<i class="fa fa-circle fa-stack-2x"></i>
+					<i class="fa fa-columns fa-stack-1x fa-inverse"></i>
+				</span>
 				<h4>Widgets</h4>
 				<p>Widgets - the mini content boxes of a page. Others call it "Modules"(Joomla), "Blocks"(Drupal), and also "Widgets"(Wordpress). Extend the functionality by themes using unlimited regions.</p>
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-md-4">
-				<i class="fa fa-group fa-3x"></i>
+				<span class="fa-stack fa-3x">
+					<i class="fa fa-circle fa-stack-2x"></i>
+					<i class="fa fa-group fa-stack-1x fa-inverse"></i>
+				</span>
 				<h4>Users & Groups</h4>
 				<p>Users, Roles, and Permissions. Users on your site can be assigned permissions via roles. Permissions are quite flexible -- you are allowed to assign permission for any task to any role, depending on the needs of your site. </p>
 			</div>
 			<div class="col-md-4">
-				<i class="fa fa-folder-open fa-3x"></i>
+				<span class="fa-stack fa-3x">
+					<i class="fa fa-circle fa-stack-2x"></i>
+					<i class="fa fa-folder-open fa-stack-1x fa-inverse"></i>
+				</span>
 				<h4>Taxonomy & Tags</h4>
 				<p>Gleez uses <a href="http://www.sitepoint.com/hierarchical-data-database-2/">MPTT</a> system for classifying content, which is known as taxonomy and implemented in the core. You can define your own vocabularies (groups of taxonomy terms), and add terms to each vocabulary.</p>
 			</div>
 			<div class="col-md-4 seo">
-				<i class="fa fa-globe fa-3x"></i>
+				<span class="fa-stack fa-3x">
+					<i class="fa fa-circle fa-stack-2x"></i>
+					<i class="fa fa-globe fa-stack-1x fa-inverse"></i>
+				</span>
 				<h4>SEO</h4>
 				<p> Canonical URL: the search engine friendly URL that you want the search engines to treat as authoritative. Clean url's, canocial & shortlinks are in core for SEO. Gleez adds meta tags support and url aliases handled by path class.</p>
 			</div>
