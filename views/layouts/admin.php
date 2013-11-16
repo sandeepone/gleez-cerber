1e674ae75cf121aeb14a49929218823936e02338
Sergey Yakovlev <sadhooklay@gmail.com>
2013-11-16 22:31:04 +0400

    Fixed issue: Fixed links to js for IE

--- views/layouts/admin.php
+++ views/layouts/admin.php
@@ -7,8 +7,8 @@
 	<?php echo Meta::links(); ?>
 	<!-- HTML5 shim and Respond.js, for IE6-8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
-		<script src="/media/js/html5.js"></script>
-		<script src="/media/js/respond.min.js"></script>
+		<?php echo HTML::script('media/js/html5.js', NULL, TRUE); ?>
+		<?php echo HTML::script('media/js/respond.min.js', NULL, TRUE); ?>
 	<![endif]-->
 	<?php echo Assets::css(); ?>
 </head>
