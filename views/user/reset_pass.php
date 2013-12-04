843f98bbcaeeb74a53676e603ce5247a7963159c
Sergey Yakovlev <sadhooklay@gmail.com>
2013-12-05 01:56:47 +0400

    Fixed issue: Removed extra indent for whole HTML

--- views/user/reset_pass.php
+++ views/user/reset_pass.php
@@ -1,28 +1,28 @@
-	<div class="col-sm-6 col-sm-offset-3">
-		<?php include Kohana::find_file('views', 'errors/partial'); ?>
+<div class="col-sm-6 col-sm-offset-3">
+	<?php include Kohana::find_file('views', 'errors/partial'); ?>
 
-		<div class="panel panel-default window-shadow">
-			<div class="panel-heading">
-				<h3 class="panel-title">
-					<?php _e('Enter email specified at registration for password reset')?>
-				</h3>
-			</div>
-			<?php echo Form::open($action, array('class' => 'form form-horizontal', 'role' => 'form')) ?>
-				<div class="panel-body">
-					<div class="form-group <?php echo isset($errors['mail']) ? 'has-error': ''; ?>">
-						<div class="col-md-12">
-							<div class="input-group">
-								<span class="input-group-addon"><i class="fa fa-envelope-o fa-fw"></i></span>
-								<?php echo Form::input('mail', $post['mail'], array('class' => 'form-control input-lg')); ?>
-							</div>
-						</div>
-					</div>
-					<div class="form-group ab-wrapper">
-						<div class="col-md-12">
-							<?php echo Form::button('reset_pass', __('Reset'), array('class' => 'btn btn-danger pull-right', 'type' => 'submit'))?>
+	<div class="panel panel-default window-shadow">
+		<div class="panel-heading">
+			<h3 class="panel-title">
+				<?php _e('Enter email specified at registration for password reset')?>
+			</h3>
+		</div>
+		<?php echo Form::open($action, array('class' => 'form form-horizontal', 'role' => 'form')) ?>
+			<div class="panel-body">
+				<div class="form-group <?php echo isset($errors['mail']) ? 'has-error': ''; ?>">
+					<div class="col-md-12">
+						<div class="input-group">
+							<span class="input-group-addon"><i class="fa fa-envelope-o fa-fw"></i></span>
+							<?php echo Form::input('mail', $post['mail'], array('class' => 'form-control input-lg')); ?>
 						</div>
 					</div>
 				</div>
-			<?php echo Form::close() ?>
-		</div>
+				<div class="form-group ab-wrapper">
+					<div class="col-md-12">
+						<?php echo Form::button('reset_pass', __('Reset'), array('class' => 'btn btn-danger pull-right', 'type' => 'submit'))?>
+					</div>
+				</div>
+			</div>
+		<?php echo Form::close() ?>
 	</div>
+</div>
