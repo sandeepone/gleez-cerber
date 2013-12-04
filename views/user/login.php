b338fde32461a68a323450fa9d5c5caa39ad7a30
Sergey Yakovlev <sadhooklay@gmail.com>
2013-12-04 19:08:24 +0400

    Fixed issue: Cleanup Login page

--- views/user/login.php
+++ views/user/login.php
@@ -1,45 +1,64 @@
-<?php Assets::css('user', 'media/css/user.css', array('weight' => 2)); ?>
+<?php
+	// @todo Should be moved to controller
+	Assets::css('user', 'media/css/user.css', array('weight' => 2));
+?>
 
-<div class="account-container col-md-4 window-shadow">
+<div class="col-sm-4 col-sm-offset-4">
 	<?php include Kohana::find_file('views', 'errors/partial'); ?>
 
-	<div class="content clearfix">
-		<?php echo Form::open($action, array('class' => 'row', 'role' => 'form')); ?>
-			<h1><?php echo $site_name ?></h1>
-
-			<div class="login-fields">
-				<p><?php echo __('Sign in using your registered account'); ?></p>
-
-				<div class="form-group <?php echo isset($errors['name']) ? 'has-error': ''; ?>">
-					<?php echo Form::label('name', __('Username/Email'), array('class' => 'sr-only control-label')) ?>
-					<?php echo Form::input('name', $post->name, array('class' =>'form-control', 'placeholder' => __('Username/Email'))); ?>
-				</div>
-
-				<div class="form-group <?php echo isset($errors['password']) ? 'has-error': ''; ?>">
-					<?php echo Form::label('name', __('Password'), array('class' => 'sr-only control-label')) ?>
-					<?php echo Form::password('password', NULL, array('class' =>'form-control', 'placeholder' => __('Password'))); ?>
-				</div>
-			</div>
-
-			<div class="login-actions">
-				<?php echo Form::submit('login', __('Login'), array('class' => 'btn btn-primary btn-lg btn-block', 'type' => 'submit')); ?>
-				<div class="clearfix"></div><br>
-				<span class="login-checkbox">
-					<input id="remember" name="remember" type="checkbox" class="field login-checkbox" value="TRUE" tabindex="4">
-					<label class="choice" for="remember"><?php echo __('Stay Signed in'); ?></label>
-				</span>
-				<?php echo HTML::anchor('user/reset/password', __('Forgot Password?'), array('class' => 'pull-right')); ?>
-				<div class="clearfix"></div><br>
-
-			 	<?php if ($register): ?>
-			 		<div class="text-center">
-						<?php echo HTML::anchor('user/register', __("Don't have an account?")); ?>
-					</div>
-				<?php endif; ?>
-			</div>
-
-			<?php if ($providers): ?>
+	<div class="panel panel-default window-shadow">
+		<div class="panel-heading">
+			<h1 class="panel-title"><?php echo $site_name ?></h1>
+		</div>
+		<?php echo Form::open($action, array('class' => 'form form-horizontal', 'role' => 'form')); ?>
+			<div class="panel-body">
 				<div class="form-group">
+					<div class="col-sm-12">
+						<p><?php echo __('Sign in using your registered account'); ?></p>
+					</div>
+				</div>
+				<div class="form-group <?php echo isset($errors['name']) ? 'has-error': ''; ?>">
+					<div class="col-sm-12">
+						<?php echo Form::label('name', __('Username/Email'), array('class' => 'sr-only control-label')) ?>
+						<?php echo Form::input('name', $post->name, array('class' =>'form-control', 'placeholder' => __('Username/Email'))); ?>
+					</div>
+				</div>
+				<div class="form-group <?php echo isset($errors['password']) ? 'has-error': ''; ?>">
+					<div class="col-sm-12">
+						<?php echo Form::label('name', __('Password'), array('class' => 'sr-only control-label')) ?>
+						<?php echo Form::password('password', NULL, array('class' =>'form-control', 'placeholder' => __('Password'))); ?>
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="col-sm-12">
+						<div class="checkbox">
+							<label for="remember">
+								<input id="remember" name="remember" type="checkbox" class="field login-checkbox" value="1" tabindex="4">&nbsp;
+								<?php _e('Stay Signed in'); ?>
+							</label>
+						</div>
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="col-sm-12">
+						<?php echo Form::submit('login', __('Login'), array('class' => 'btn btn-primary btn-lg btn-block', 'type' => 'submit')); ?>
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="col-sm-6">
+						<?php echo HTML::anchor('user/reset/password', __('Forgot Password?')); ?>
+					</div>
+					<?php if ($register): ?>
+						<div class="col-sm-6">
+							<?php echo HTML::anchor('user/register', __("Don't have an account?"), array('class' => 'pull-right')); ?>
+						</div>
+					<?php endif; ?>
+				</div>
+			</div>
+		<?php echo Form::close() ?>
+		<?php if ($providers): ?>
+			<ul class="list-group">
+				<li class="list-group-item">
 					<p><?php echo __('Sign in using social network:');?></p>
 					<div class="btn-group">
 						<?php
@@ -64,9 +83,8 @@
 						?>
 					</div>
 					<p><small><?php echo __('Fast, safe & secure way!');?></small></p>
-				</div>
-			<?php endif; ?>
-
-		<?php echo Form::close() ?>
+				</li>
+			</ul>
+		<?php endif; ?>
 	</div>
 </div>
