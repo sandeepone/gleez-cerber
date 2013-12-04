f74374187db1988a811a7e99b53cc9925996c3b8
Sergey Yakovlev <sadhooklay@gmail.com>
2013-12-05 01:11:55 +0400

    Fixed issue: Confirm Reset Password page in accordance with BS3

--- views/user/confirm_reset_password.php
+++ views/user/confirm_reset_password.php
@@ -1,34 +1,39 @@
-<div class="account-container">
+<div class="col-sm-6 col-sm-offset-3">
 	<?php include Kohana::find_file('views', 'errors/partial'); ?>
 
-	<div class="content clearfix">
-		<?php echo Form::open($action, array('class' => 'row-fluid form-password')) ?>
-			<h1><?php echo __('Change Password') ?></h1>
-
-			<div class="login-fields">
-				<div class="field">
-					<div class="control-group <?php echo isset($errors['pass']) ? 'has-error': ''; ?>">
-						<?php echo Form::label('pass', __('New password'), array('class' => 'control-label')); ?>
-						<div class="controls">
-							<div class="input-prepend">
-								<span class="add-on"><i class="fa fa-key"></i></span>
-								<?php echo Form::password('pass', NULL); ?>
-							</div>
-						</div>
-					</div>
-
-					<div class="control-group <?php echo isset($errors['pass_confirm']) ? 'has-error': ''; ?>">
-						<?php echo Form::label('pass_confirm', __('New password (again)'), array('class' => 'control-label')); ?>
-						<div class="controls">
-							<div class="input-prepend">
-								<span class="add-on"><i class="fa fa-key"></i></span>
-								<?php echo Form::password('pass_confirm', NULL); ?>
-							</div>
+	<div class="panel panel-default window-shadow">
+		<div class="panel-heading">
+			<h3 class="panel-title">
+				<?php _e('Change Password')?>
+			</h3>
+		</div>
+		<?php echo Form::open($action, array('class' => 'form form-horizontal', 'role' => 'form')) ?>
+			<div class="panel-body">
+				<div class="form-group <?php echo isset($errors['pass']) ? 'has-error': ''; ?>">
+					<?php echo Form::label('pass', __('New password'), array('class' => 'col-sm-4 control-label')); ?>
+					<div class="col-sm-8">
+						<div class="input-group">
+							<span class="input-group-addon"><i class="fa fa-key"></i></span>
+							<?php echo Form::password('pass', NULL, array('class' => 'form-control')); ?>
 						</div>
 					</div>
 				</div>
-				<hr>
-				<?php echo Form::submit('password_confirm', __('Apply new password'), array('class' => 'btn btn-inverse pull-right')); ?>
+
+				<div class="form-group <?php echo isset($errors['pass_confirm']) ? 'has-error': ''; ?>">
+					<?php echo Form::label('pass_confirm', __('New password (again)'), array('class' => 'col-sm-4 control-label')); ?>
+					<div class="col-sm-8">
+						<div class="input-group">
+							<span class="input-group-addon"><i class="fa fa-key"></i></span>
+							<?php echo Form::password('pass_confirm', NULL, array('class' => 'form-control')); ?>
+						</div>
+					</div>
+				</div>
+
+				<div class="form-group ab-wrapper">
+					<div class="col-md-12">
+						<?php echo Form::button('password_confirm', __('Apply new password'), array('class' => 'btn btn-primary pull-right', 'type' => 'submit'))?>
+					</div>
+				</div>
 			</div>
 		<?php echo Form::close(); ?>
 	</div>
