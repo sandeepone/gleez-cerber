23e2d9a9b170ad19f97f3bb2f2e7e2aa75ee04c4
Sergey Yakovlev <sadhooklay@gmail.com>
2013-12-13 02:53:54 +0400

    Fixed issue: Fixed icon width for user profile.
    See dd00179

--- views/user/profile.php
+++ views/user/profile.php
@@ -8,15 +8,15 @@
 
 				<div class="list-group">
 					<a href="javascript:;" class="list-group-item">
-						<i class="fa fa-asterisk"></i> <?php echo __('Activity Feed'); ?>
-						<i class="fa fa-chevron-right list-group-chevron"></i>
+						<i class="fa fa-fw fa-asterisk"></i> <?php echo __('Activity Feed'); ?>
+						<i class="fa fa-fw fa-chevron-right list-group-chevron"></i>
 					</a>
 					<a href="javascript:;" class="list-group-item">
-						<i class="fa fa-group"></i> <?php echo __('Friends'); ?>
-						<i class="fa fa-chevron-right list-group-chevron"></i>
+						<i class="fa fa-fw fa-group"></i> <?php echo __('Friends'); ?>
+						<i class="fa fa-fw fa-chevron-right list-group-chevron"></i>
 						<span class="badge">7</span>
 					</a>
-					<?php echo HTML::anchor('user/edit', '<i class="fa fa-cog"></i> '.__('Settings') .'<i class="fa fa-chevron-right list-group-chevron"></i>' , array('class' => 'list-group-item')); ?>
+					<?php echo HTML::anchor('user/edit', '<i class="fa fa-fw fa-cog"></i> '.__('Settings') .'<i class="fa fa-chevron-right list-group-chevron"></i>' , array('class' => 'list-group-item')); ?>
 				</div>
 			</div>
 
@@ -29,19 +29,19 @@
 				<div class="col-md-12 vcard-details">
 					<?php if ($is_owner OR User::is_admin()): ?>
 						<dl title="<?php echo __('Email') ?>">
-							<dt><i class="fa fa-envelope"></i></dt>
+							<dt><i class="fa fa-fw fa-envelope"></i></dt>
 							<dd><a class="email" data-email="<?php echo $user->mail ?>" href="mailto:<?php echo $user->mail ?>"><?php echo $user->mail ?></a></dd>
 						</dl>
 					<?php endif; ?>
 					<?php if ($user->homepage): ?>
 						<dl title="<?php echo __('Home Page') ?>">
-							<dt><i class="fa fa-globe"></i></dt>
+							<dt><i class="fa fa-fw fa-globe"></i></dt>
 							<dd><?php echo HTML::anchor($user->homepage, $user->homepage, array('itemprop' => 'url')); ?></dd>
 						</dl>
 					<?php endif; ?>
 
 					<dl title="<?php echo __('Birthday') ?>">
-						<dt><i class="fa fa-calendar"></i></dt>
+						<dt><i class="fa fa-fw fa-calendar"></i></dt>
 						<dd>
 							<time itemprop="birthDate" content="<?php echo Date::date_format($user->dob, DateTime::ISO8601)?>" datetime="<?php echo Date::date_format($user->dob, DateTime::ISO8601)?>">
 								<?php echo Date::date_format($user->dob); ?>
@@ -50,7 +50,7 @@
 					</dl>
 					<?php if (User::is_admin()): ?>
 						<dl title="<?php echo __('User Groups') ?>">
-							<dt><i class="fa fa-group"></i></dt>
+							<dt><i class="fa fa-fw fa-group"></i></dt>
 							<dd class="tagcloud">
 								<?php foreach ($user->roles() as $role): ?>
 									<span><?php echo Text::plain(ucfirst($role)); ?></span>
