f8ba19f71c64a117734f1910eca4cf274037c349
Sandeep Sangamreddi <sandeepone@gmail.com>
2014-02-26 02:54:06 +0530

    Update feature: Cleanup admin layout

--- views/admin.tpl.php
+++ views/admin.tpl.php
@@ -10,27 +10,30 @@
 		<?php endif; ?>
 		<!-- ########## Content start ############# -->
 		<div class="col-sm-9 col-md-10 main-content">
+			<div class="wrapper">
 				<?php if ($title): ?>
 					<div class="page-header">
 						<h1> <i class="fa fa-info-circle"></i> <?php echo $title;?></h1>
 					</div>
 				<?php endif; ?>
-			<div class="content-wrapper">
-				<?php if ($messages): ?>
-					<!-- ########## Messages start ########## -->
-					<div id="messages" class="messages">
-						<?php echo $messages ?>
-					</div>
-					<!-- ########## Messages end ########## -->
-				<?php endif; ?>
+				<div class="content-wrapper">
+					<?php if ($messages): ?>
+						<!-- ########## Messages start ########## -->
+						<div id="messages" class="messages">
+							<?php echo $messages ?>
+						</div>
+						<!-- ########## Messages end ########## -->
+					<?php endif; ?>
 
-				<?php if ($tabs): ?>
-					<div id="tabs-actions">
-						<div id="tabs"><?php echo $tabs; ?></div>
+					<?php if ($tabs): ?>
+						<div id="tabs-actions">
+							<div id="tabs"><?php echo $tabs; ?></div>
+						</div>
+					<?php endif; ?>
+
+					<div id="content-body" class="<?php echo $tabs ? 'with-tabs' : 'without-tabs'?>">
+						<?php echo $content; ?>
 					</div>
-				<?php endif;?>
-				<div id="content-body" class="<?php echo $tabs ? 'with-tabs' : 'without-tabs'?>">
-					<?php echo $content; ?>
 				</div>
 			</div>
 		</div>
